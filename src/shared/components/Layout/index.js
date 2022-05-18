d71378bafa2ed2f58bb18672312910000766e54f
Hugo Oliveira Lamounier <hugolamo@gmail.com>
2022-05-18 16:53:24 -0300

    Remove Navbar

--- src/shared/components/Layout/index.js
+++ src/shared/components/Layout/index.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 function Layout({ children }) {
   return (
     <div className="container">
-      <Navbar />
+      {/*<Navbar />*/}
       <div>{children}</div>
     </div>
   );
