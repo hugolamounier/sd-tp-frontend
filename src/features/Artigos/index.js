23420c8de6aa12f0d54ce62c7f3a631856474dc9
Hugo Oliveira Lamounier <hugolamo@gmail.com>
2022-05-18 17:48:39 -0300

    Add post article

--- src/features/Artigos/index.js
+++ src/features/Artigos/index.js
@@ -1,8 +1,68 @@
 import React from 'react';
 import Layout from '../../shared/components/Layout';
+import ArtigoService from '../../services/ArtigoService';
 
 function Artigos() {
-  return <Layout>1233</Layout>;
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    try {
+      await ArtigoService.insertArtigo({ formData });
+      alert('Artigo inserido com sucesso!');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Layout>
+      <h1>Cadastro de Artigo</h1>
+      <form onSubmit={(e) => handleOnSubmit(e)}>
+        <div className="mb-3">
+          <label htmlFor="titulo" className="form-label">
+            Título
+          </label>
+          <input type="text" className="form-control" id="titulo" name="titulo" required />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="autor" className="form-label">
+            Autor
+          </label>
+          <input type="text" className="form-control" id="autor" name="autor" required />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="descricao" className="form-label">
+            Descrição
+          </label>
+          <textarea className="form-control" id="descricao" name="descricao" required />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="anoPublicacao" className="form-label">
+            Ano de publicação
+          </label>
+          <input
+            type="number"
+            minLength={4}
+            maxLength={4}
+            className="form-control"
+            id="anoPublicacao"
+            name="anoPublicacao"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="url" className="form-label">
+            URL do artigo
+          </label>
+          <input type="text" className="form-control" id="url" name="url" required />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Salvar
+        </button>
+      </form>
+    </Layout>
+  );
 }
 
 export default Artigos;
---
+++ src/services/ArtigoService.js
@@ -0,0 +1,18 @@
+class ArtigoService {
+  static API_URL = 'http://127.0.0.1:8000/artigos';
+
+  static async insertArtigo({ formData }) {
+    var response = await fetch(`${this.API_URL}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: formData,
+    });
+
+    return await response.json();
+  }
+}
+
+export default ArtigoService;
